83faab9eb6af470401a17cbef20b3580968c19ac
Jhoe8 <red.mrjhon8@gmail.com>
2018-03-29 08:33:51 +0800

    BREAK

---
+++ assets/js/theme.js
@@ -0,0 +1,15 @@
+
+$( document ).ready( function(){
+		var availableTags = [
+      "Nowhereville XX 00000",
+      "Nowhereville XX 00000",
+      "Nowhereville XX 00000",
+      "Nowhereville XX 00000",
+      "Nowhereville XX 00000",
+      "Nowhereville XX 00000",
+    ];
+	
+	$('#zipCity').autocomplete({
+		source: availableTags
+	});
+});
