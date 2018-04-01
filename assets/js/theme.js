d4f1c351449296595b1b7d4357588b96b138bac0
Joegie <red.mrjhon8@gmail.com>
2018-04-02 06:47:43 +0800

    BREAK

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -2,14 +2,14 @@
 $( document ).ready( function(){
 		var availableTags = [
       "Nowhereville XX 00000",
-      "Nowhereville XX 00000",
-      "Nowhereville XX 00000",
-      "Nowhereville XX 00000",
-      "Nowhereville XX 00000",
-      "Nowhereville XX 00000",
+      "Nowhereville XX 12345",
+      "Nowhereville XX 23456",
+      "Nowhereville XX 34567",
+      "Nowhereville XX 45678",
+      "Nowhereville XX 56789",
     ];
 	
-	$('#zipCity').autocomplete({
-		source: availableTags 
+	$('#zipCity').autocomplete({ //$, (), !
+		source: availableTags
 	});
 });
